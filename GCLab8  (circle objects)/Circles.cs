1b4d45ad564a49ee3b8f4ab364ec272113f26889
elijah <elireid.28@gmail.com>
2024-02-04 19:47:44 -0500

    first

---
+++ GCLab8  (circle objects)/Circles.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCLab8___circle_objects_
+{
+    internal class Circle
+    {
+        //properties
+        private double radius;
+
+        //constructor
+        public Circle(double _radius)
+        {
+            radius = _radius;
+        }
+        public double CalculateDiameter()
+        {
+            return radius * 2;
+        }
+        public double CalculateCircumference()
+        {
+            return (Math.PI * 2 * radius);
+        }
+        public double CalculateArea()
+        {
+            return Math.PI * (radius * radius);
+        }
+        public void Grow()
+        {
+            radius *= 2;
+        }
+        public double GetRadius()
+        {
+            return radius;
+        }
+    }
+}
---
+++ GCLab8  (circle objects)/Program.cs
@@ -0,0 +1,45 @@
+﻿using GCLab8___circle_objects_;
+
+//welcome
+Console.WriteLine("Welcome to the Circle Tester!");
+
+//get user input + validate it
+Console.Write("Enter radius: ");
+double tempRadius = 0;
+try
+{
+    tempRadius = double.Parse(Console.ReadLine().Trim());
+}
+catch (Exception)
+{
+   Console.WriteLine("Invalid, please try again");
+    tempRadius = double.Parse(Console.ReadLine().Trim());
+}
+
+Circle choice = new Circle(tempRadius);
+
+//loop calcluations if user wants
+bool runCalcs = true;
+while (runCalcs)
+{
+
+    //output calculations
+    Console.Write($"Diameter: {choice.CalculateDiameter()}\n");
+    Console.Write($"Circumference: {choice.CalculateCircumference()}\n");
+    Console.Write($"Area: {choice.CalculateArea()}\n");
+
+    //check & see if user would like to circle to grow
+    Console.WriteLine("Would you like the circle to grow? y/n");
+    string growChoice = Console.ReadLine().ToLower().Trim();
+    if (growChoice == "y")
+    {
+        choice.Grow();
+    }
+    else
+    {
+        Console.WriteLine($"Your circle's radius is {choice.GetRadius()}");
+        Console.WriteLine("Goodbye!");
+        break;
+    }
+
+}
